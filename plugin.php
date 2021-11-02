92ff423a8ac41c81a5f05f1ae36407c9d8a3d78c
Ryan Welcher <ryan.welcher@automattic.com>
2021-11-02 15:51:29 -0400

    Add custom page with placeholder markup

--- plugin.php
+++ plugin.php
@@ -7,5 +7,25 @@
  * Author URI: https://www.wordpress.org
  * Text Domain: theme-json-builder
  *
- * @package theme-json-builder
+ * @package ThemeJsonBuilder
  */
+
+namespace ThemeJsonBuilder;
+
+use ThemeJsonBuilder\Admin;
+
+/**
+ * Helpful constants
+ */
+define( 'THEM_JSON_BUILDER_DIR_PATH', plugin_dir_path( __FILE__ ) );
+define( 'THEM_JSON_BUILDER_INC_PATH', THEM_JSON_BUILDER_DIR_PATH . '/includes/' );
+
+
+// Require the files.
+require_once THEM_JSON_BUILDER_INC_PATH . 'admin-page.php';
+
+
+/**
+ * Entry point for the plugin.
+ */
+Admin\init();
