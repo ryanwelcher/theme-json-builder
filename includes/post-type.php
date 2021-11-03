caba4522e0cb2b0de72137421481249fd9f31694
Ryan Welcher <ryan.welcher@automattic.com>
2021-11-03 15:08:32 -0400

    Remove all core blocks and create filter for allowed custom blocks.

--- includes/post-type.php
+++ includes/post-type.php
@@ -12,6 +12,7 @@ namespace ThemeJsonBuilder\PostType;
  */
 function init() {
 	\add_action( 'init', __NAMESPACE__ . '\register_custom_post_type' );
+	\add_filter( 'allowed_block_types_all', __NAMESPACE__ . '\remove_core_blocks', 10, 2 );
 }
 
 
@@ -33,3 +34,32 @@ function register_custom_post_type() {
 		)
 	);
 }
+
+/**
+ * Callback to remove all of the core blocks.
+ *
+ * @param array                   $allowed_block_types The list of registered blocks.
+ * @param WP_Block_Editor_Context $editor_context      The current block editor context.
+ */
+function remove_core_blocks( $allowed_block_types, $editor_context ) {
+	// Remove all the core blocks from this post type.
+	if ( 'theme-json' === $editor_context->post->post_type ) {
+		// Return the array of custom blocks we will allow.
+		return get_allowed_custom_block_list();
+	}
+
+	// Return all registered blocks.
+	return $allowed_block_types;
+}
+
+/**
+ * Helper to return the list of custom blocks we'll use.
+ *
+ * @todo Get tricksy in here to scan the files and create the list that way.
+ *
+ * @return array The list of custom blocks.
+ */
+function get_allowed_custom_block_list() {
+	return apply_filters( 'theme_json_builder_allowed_blocks', array() );
+}
+
