024d05bc364c7f9896d6b5174692f6a1a9be3774
Ryan Welcher <ryan.welcher@automattic.com>
2021-12-13 13:50:21 -0500

    Display the curly braces around the inner blocks. Add controls to the theme.json block

--- includes/blocks/theme-json/edit.js
+++ includes/blocks/theme-json/edit.js
@@ -6,13 +6,75 @@ import {
 	InnerBlocks,
 	InspectorControls,
 } from '@wordpress/block-editor';
+import {
+	PanelBody,
+	PanelRow,
+	SelectControl,
+	TextControl,
+} from '@wordpress/components';
+import { useDispatch } from '@wordpress/data';
+import { __ } from '@wordpress/i18n';
 
-const Edit = ( { setAttributes } ) => {
-	const blockProps = useBlockProps();
+/**
+ * Internal dependencies
+ */
+import SourceCodeDisplay from '../../components/source-code-display';
+import { STORE_NAME } from '../../datastore/constants';
+
+const Edit = ( {
+	attributes: { objectPath, schema, version },
+	setAttributes,
+} ) => {
+	const blockProps = useBlockProps( {
+		className: 'has-extra-small-font-size', // This might be an issue
+	} );
+
+	const { updateThemeJSON } = useDispatch( STORE_NAME );
+
+	const topCode = `{\n\t"$schema": "${ schema }",\n\t"version": ${ version }`;
+
 	return (
 		<div { ...blockProps }>
-			<InspectorControls>Version: 1</InspectorControls>
-			<InnerBlocks templateLock={ false } />
+			<SourceCodeDisplay lang="json" sourceCode={ topCode } />
+			<InnerBlocks />
+			<SourceCodeDisplay lang="json" sourceCode={ '}' } />
+			<InspectorControls>
+				<PanelBody title="Options" initialOpen={ true }>
+					<PanelRow>
+						<TextControl
+							label={ __( 'Schema', 'theme-json-builder' ) }
+							value={ schema }
+							onChange={ ( newSchema ) => {
+								setAttributes( { schema: newSchema } );
+								updateThemeJSON(
+									objectPath,
+									'schema',
+									newSchema
+								);
+							} }
+						/>
+					</PanelRow>
+					<PanelRow>
+						<SelectControl
+							label={ __( 'Version', 'theme-json-builder' ) }
+							labelPosition="side"
+							options={ [
+								{ label: '1 - deprecated', value: 1 },
+								{ label: '2', value: 2 },
+							] }
+							value={ version }
+							onChange={ ( newVersion ) => {
+								setAttributes( { version: newVersion } );
+								updateThemeJSON(
+									objectPath,
+									'version',
+									newVersion
+								);
+							} }
+						/>
+					</PanelRow>
+				</PanelBody>
+			</InspectorControls>
 		</div>
 	);
 };
